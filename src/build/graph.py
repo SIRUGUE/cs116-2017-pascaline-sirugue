f215c20a8e74bd694ec95eea1987c34af7a4c474
Sirugue Pascaline Louise Isabelle <sirugue@SB-CSEA-2017026.intranet.epfl.ch>
2017-10-30 23:00:56 +0100

    network modified in order to make the graph

---
+++ src/build/graph.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data1= np.genfromtxt ('test.txt', delimiter=',')
+data2= np.genfromtxt ('spikes.txt', delimiter=',')
+
+time1= data1[:,][:,0]
+neuron= data1[:,][:,1]
+time2= data2[:,][:,0]
+spikes= data2[:,][:,1]
+
+a=plt.subplot(2,1,1)
+plt.scatter(time1, neuron, s=5, c='blue')
+plt.ylim(0,50)
+plt.ylabel('neuron number')
+plt.xlabel('time in ms')
+
+
+
+b=plt.subplot (2,1,2, sharex=a)
+plt.plot(time2, spikes, c='blue')
+plt.ylabel('number of spikes')
+plt.xlabel('time in ms')
+plt.show()
